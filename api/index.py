f898a19d0fb53edb34de996a27cea1e1c1538b51
Nihad Nahmatli <bakueduaz@gmail.com>
2024-09-14 16:49:28 +0000

    commit

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from .config import API_KEY
 app = Flask(__name__)
 
 CORS(app, resources = {r"/api/news": {
-    "origins": [],
+    "origins": ["https://bakueduaz.github.io/demosite/"],
     "methods": ["GET"]
 }})
 
